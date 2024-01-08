734ecb42385d7edaa98c1a1d1ce24891ea19b3ca
AleksandraJ22 <145792385+AleksandraJ22@users.noreply.github.com>
2024-01-09 00:23:36 +0100

    Update appTests.py

--- appTests.py
+++ appTests.py
@@ -13,7 +13,7 @@ categoria_begudes=Categoria('Begudes',{'Begudes':[Producte('Coca-Cola', 5), Prod
 categoria_lactis=Categoria('Lactis', {'Lactis':[Producte('Leche', 1), Producte('Iogurts', 4)]})
 categoria_neteja=Categoria('Neteja', {'Neteja': [Producte('Detergent', 3), Producte('Esponjas', 2)]})
 categoria_fiv= Categoria('Fruita i verdures',{'Fruita i verdures': [Producte('Fruita', 6), Producte('Verduras', 8)]})
-categoria_animal = Categoria('Animal', {'Animal': [Producte('Pienso', 3), Producte('Snacks', 5)]})
+categoria_animal = Categoria('Animal', {'Animal': []})
 miApp.afegir_categoria(categoria_frescos)   
 miApp.afegir_categoria(categoria_begudes) 
 miApp.afegir_categoria(categoria_lactis) 
@@ -39,7 +39,13 @@ def testBorrarCategoria():
     
 def testAñadirProducteACategoria():
     print('Añadimos un producto a la categoria Animal')
-    assert( miApp.afegir_Producte_a_categoria(categoria_animal,Producte('atun_gatos',3)) == True )
+    atun_gato=Producte('atun_gatos',3)
+    assert(miApp.agregarProducto(categoria_animal,atun_gato) == True)
+    miApp.imprimirCategorias()
+    
+def testFiltrarPorCategoria():
+    pass
+    
     
     
     
